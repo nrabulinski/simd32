2f9f91a55056a38517f297338e434a1d900978eb
Nikodem Rabuliński <nikodemrabulinski@gmail.com>
2020-03-05 07:40:43 +0100

    i am speed

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,15 +4,19 @@ use criterion::{
 use base32::decode;
 
 pub fn base32_decode(c: &mut Criterion) {
-    let input = std::fs::read("/home/niko/projects/base32/testcase16mb").unwrap();
-    c.bench_function("base32 decode (26M in, 16M out)", |b| {
-        b.iter(|| decode(&input))
-    });
     let input = std::fs::read("/home/niko/projects/base32/testcase").unwrap();
     c.bench_function("base32 decode (320K in, 200K out)", |b| {
         b.iter(|| decode(&input))
     });
+    let input = std::fs::read("/home/niko/projects/base32/testcase16mb").unwrap();
+    c.bench_function("base32 decode (26M in, 16M out)", |b| {
+        b.iter(|| decode(&input))
+    });
+    let input = std::fs::read("/home/niko/projects/base32/testcase200mb").unwrap();
+    c.bench_function("base32 decode (321M in, 200M out)", |b| {
+        b.iter(|| decode(&input))
+    });
 }
 
-criterion_group!(benches, base32_decode);
+criterion_group!(name = benches; config = Criterion::default().sample_size(20); targets = base32_decode);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -6,25 +6,24 @@ macro_rules! decode_func {
     ($($name:ident: $n:literal),+) => {
         $(
         #[inline]
-        fn $name(input: &[u8], res: &mut [u8], mut buf: [u8; 64]) {
+        fn $name(input: &[u8], res: &mut [u8]) {
             assert!(input.len() >= $n);
             let n = $n / 8;
             assert!(res.len() >= n * 5);
             type S = Simd<[u8; $n]>;
             let v = S::from_slice_unaligned(input);
             let m = v.ge(S::splat(0b1000000));
+            let v = m.select((v & 0x5f) - 0b1000001, v ^ 0b101000);
             unsafe {
-                m.select((v & 0x5f) - 0b1000001, v ^ 0b101000)
-                    .write_to_slice_aligned_unchecked(&mut buf);
-            }
-            for index in 0..n {
-                let i = index * 5;
-                let x = index * 8;
-                res[i + 0] = (buf[x + 0] << 3) | (buf[x + 1] >> 2);
-                res[i + 1] = (buf[x + 1] << 6) | (buf[x + 2] << 1) | (buf[x + 3] >> 4);
-                res[i + 2] = (buf[x + 3] << 4) | (buf[x + 4] >> 1);
-                res[i + 3] = (buf[x + 4] << 7) | (buf[x + 5] << 2) | (buf[x + 6] >> 3);
-                res[i + 4] = (buf[x + 6] << 5) |  buf[x + 7];
+                for index in 0..n {
+                    let i = index * 5;
+                    let x = index * 8;
+                    res[i + 0] = (v.extract_unchecked(x + 0) << 3) | (v.extract_unchecked(x + 1) >> 2);
+                    res[i + 1] = (v.extract_unchecked(x + 1) << 6) | (v.extract_unchecked(x + 2) << 1) | (v.extract_unchecked(x + 3) >> 4);
+                    res[i + 2] = (v.extract_unchecked(x + 3) << 4) | (v.extract_unchecked(x + 4) >> 1);
+                    res[i + 3] = (v.extract_unchecked(x + 4) << 7) | (v.extract_unchecked(x + 5) << 2) | (v.extract_unchecked(x + 6) >> 3);
+                    res[i + 4] = (v.extract_unchecked(x + 6) << 5) |  v.extract_unchecked(x + 7);
+                }
             }
         }
         )+
@@ -32,18 +31,37 @@ macro_rules! decode_func {
 }
 
 decode_func!(
-    decode_len_64: 64,
-    decode_len_32: 32,
+    //decode_len_64: 64,
+    //decode_len_32: 32,
     decode_len_16: 16
 );
+
+#[inline]
+fn decode_len_8(input: &[u8], output: &mut [u8]) {
+                let v = u8x8::from_slice_unaligned(input);
+                let m = v.ge(u8x8::splat(0b1000000));
+                let v = m.select((v & 0x5f) - 0b1000001, v ^ 0b101000);
+                unsafe {
+                    output[0] = (v.extract_unchecked(0) << 3) | (v.extract_unchecked(1) >> 2);
+                    output[1] = (v.extract_unchecked(1) << 6) | (v.extract_unchecked(2) << 1) | (v.extract_unchecked(3) >> 4);
+                    output[2] = (v.extract_unchecked(3) << 4) | (v.extract_unchecked(4) >> 1);
+                    output[3] = (v.extract_unchecked(4) << 7) | (v.extract_unchecked(5) << 2) | (v.extract_unchecked(6) >> 3);
+                    output[4] = (v.extract_unchecked(6) << 5) |  v.extract_unchecked(7);
+                }
+}
 
 pub fn decode(input: &[u8]) -> Vec<u8> {
     assert!(input.len() >= 8);
     assert!(input.len() % 8 == 0);
-    let data_len = if let Some(n) = input.iter().position(|&a| a == b'=') { n } else { input.len() };
-    let len = data_len * 5 / 8;
+    let pad_len = {
+        let v = u8x8::from_slice_unaligned(input);
+        let m = v.eq(u8x8::splat(0x3d));
+        m.select(u8x8::splat(1), u8x8::splat(0)).wrapping_sum()
+        //(v ^ 0x3d).trailing_zeros().wrapping_sum() / 8
+    } as usize;
+    let len = (input.len() - pad_len) * 5 / 8;
     let mut out = vec![0u8; input.len() / 8 * 5];
-    const DATA_CHUNK: usize = 1024;
+    const DATA_CHUNK: usize = 1024 * 3;
     const OUT_CHUNK: usize = DATA_CHUNK / 8 * 5;
     if input.len() >= DATA_CHUNK {
         input.par_chunks(DATA_CHUNK).zip(out.par_chunks_mut(OUT_CHUNK)).for_each(|(input, output)| {
@@ -58,45 +76,31 @@ pub fn decode(input: &[u8]) -> Vec<u8> {
 
 #[inline]
 fn decode_internal(input: &[u8], output: &mut [u8]) {
-    let mut buf = [0u8; 64];
     let mut i_i = 0;
     let mut o_i = 0;
     loop {
         let input = &input[i_i..];
         let output = &mut output[o_i..];
         match input.len() {
-            65.. => {
-                decode_len_64(input, output, buf);
-                i_i += 64; o_i += 40;
-            },
-            64 => return decode_len_64(input, output, buf),
-            33..=63 => {
-                decode_len_32(input, output, buf);
-                i_i += 32; o_i += 20;
-            },
-            32 => return decode_len_32(input, output, buf),
-            17..=31 => {
-                decode_len_16(input, output, buf);
+            //65.. => {
+            //    decode_len_64(input, output);
+            //    i_i += 64; o_i += 40;
+            //},
+            //64 => return decode_len_64(input, output),
+            //33.. => {
+            //    decode_len_32(input, output);
+            //    i_i += 32; o_i += 20;
+            //},
+            //32 => return decode_len_32(input, output),
+            17.. => {
+                decode_len_16(input, output);
                 i_i += 16; o_i += 10;
             },
-            16 => return decode_len_16(input, output, buf),
+            16 => return decode_len_16(input, output),
             9..=15 => {
-                panic!("Not valid base32");
-            }
-            8 => {
-                let v = u8x8::from_slice_unaligned(input);
-                let m = v.ge(u8x8::splat(0b1000000));
-                unsafe {
-                    m.select((v & 0x5f) - 0b1000001, v ^ 0b101000)
-                        .write_to_slice_aligned_unchecked(&mut buf);
-                }
-                output[0] = (buf[0] << 3) | (buf[1] >> 2);
-                output[1] = (buf[1] << 6) | (buf[2] << 1) | (buf[3] >> 4);
-                output[2] = (buf[3] << 4) | (buf[4] >> 1);
-                output[3] = (buf[4] << 7) | (buf[5] << 2) | (buf[6] >> 3);
-                output[4] = (buf[6] << 5) |  buf[7];
-                return;
+                panic!("invalid len");
             },
+            8 => return decode_len_8(input, output),
             _ => panic!("Incorrect input length!")
         }
     }
